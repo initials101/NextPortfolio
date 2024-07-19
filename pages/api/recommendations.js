845cb38fe784b4d3b20a99f3bb588f96c8b95d80
QuantumQ9 <muriithidennnis340@gmail.com>
2024-07-19 18:54:30 +0300

    update

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -2,11 +2,11 @@
 const recommendationCard = [
   {
     id: 0,
-    name: 'Shahmir Minhas',
+    name: 'Titus Kariuki',
     image: "images/shahmir.jfif",
     designation: 'React JS | NEXT JS | Node | GraphQL',
     view: "Hey everyone! I wanted to take a moment to recommend Dennis Javaid for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Dennis is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
-    linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
+    linkednURL: "https://www.linkedin.com/in/titus-kariuki-b69a79218/"
   },
   {
     id: 1,
