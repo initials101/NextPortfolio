ab3afed7a8b19993e1367a9c5b3223dd36a893d6
QuantumQ9 <muriithidennnis340@gmail.com>
2024-07-28 11:47:58 +0300

    commit

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,7 +3,7 @@ const recommendationCard = [
   {
     id: 0,
     name: 'Titus Kariuki',
-    image: "images/shahmir.jfif",
+    image: "images/kariuki.jpg",
     designation: 'PHP | Laravel | Flutter | Dart',
     view: "Hey everyone! I wanted to take a moment to recommend Dennis for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Dennis is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "https://www.linkedin.com/in/titus-kariuki-b69a79218/"
@@ -11,8 +11,8 @@ const recommendationCard = [
   {
     id: 1,
     name: 'Japheth Kiprono',
-    image: "images/sikandar.jpeg",
-    designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
+    image: "images/japheth.jpg",
+    designation: 'Web Developper | Machine Learning Engineer | Python Developer',
     view: "I had the privilege of working alongside Dennis on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Dennis's professionalism and attention to detail shone brightly. I wholeheartedly endorse Dennis for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "https://www.linkedin.com/in/kiprono-japheth/"
   },
