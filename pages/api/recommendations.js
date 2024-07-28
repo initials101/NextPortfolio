e1b8067728d6d673403ba996252dc76fc676ed10
QuantumQ9 <muriithidennnis340@gmail.com>
2024-07-28 12:12:17 +0300

    commit

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -16,22 +16,7 @@ const recommendationCard = [
     view: "I had the privilege of working alongside Dennis on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Dennis's professionalism and attention to detail shone brightly. I wholeheartedly endorse Dennis for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "https://www.linkedin.com/in/kiprono-japhet/"
   },
-  {
-    id: 2,
-    name: 'Nasir Khan',
-    image: "images/nasir.jpg",
-    designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
-    view: "I wholeheartedly recommend Dennis as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Dennis's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
-    linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
-  },
-  {
-    id: 3,
-    name: 'Muhammad Ullah',
-    image: "images/muhammad.jpeg",
-    designation: 'MERN Stack Developer at Productbox',
-    view: "I highly recommend Dennis for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
-    linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/"
-  },
+  
 ]
 export default function handler(req, res) {
   res.status(200).json(recommendationCard)
